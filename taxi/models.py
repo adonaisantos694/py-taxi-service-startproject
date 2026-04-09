dea10cbe0334c8bd509d9a212a2b8620091da20c
Adonai <adonaisoares694@gmail.com>
2026-04-09 16:04:50 -0300

    Implement django taxi 3

--- taxi/models.py
+++ taxi/models.py
@@ -7,10 +7,12 @@ class Manufacturer(models.Model):
     country = models.CharField(max_length=255)
 
 
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+
+
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-
-
-class Driver(AbstractUser):
-    license_number = models.CharField(max_length=255, unique=True)
+    # relacionamento muitos-para-muitos
+    drivers = models.ManyToManyField(Driver)
